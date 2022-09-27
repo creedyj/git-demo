ca2c9f3e9db25b987fc7a4528313abe08ac1e4ba
James Creedy <james.creedy@thehutgroup.com>
2022-09-27 10:50:38 +0100

    Added Mars Feature

--- hello.py
+++ hello.py
@@ -1,5 +1,16 @@
+import sys
 
 def hello():
+	if sys.argv[1] == "Mars":
+		hellomars()
+	else:
+		helloworld()
+
+def hellomars():
+	print("Hello, Mars")
+
+
+def helloworld():
 	print("Hello, World")
 	
 if __name__ == '__main__':
